a99fa3b5716209f793e853d5795494864a2a4ee5
Sanjaya <poudelsanjaya12@gmail.com>
2025-02-21 13:57:16 +0545

    add validation annotations to UserRegistrationRequest DTO

--- RentThis/src/main/java/com/renting/RentThis/dto/request/UserRegistrationRequest.java
+++ RentThis/src/main/java/com/renting/RentThis/dto/request/UserRegistrationRequest.java
@@ -1,4 +1,17 @@
 package com.renting.RentThis.dto.request;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+
+@Data
 public class UserRegistrationRequest {
+    @NotBlank(message = "Name is required")
+    private  String name;
+
+    @Email(message = "Please provide valid email")
+    private String email;
+
+    @NotBlank(message = "password")
+    private String password;
 }
