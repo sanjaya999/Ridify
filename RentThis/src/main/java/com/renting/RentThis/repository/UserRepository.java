6c2c698541b747f6281f52b7caa7dbe12641b842
Sanjaya <poudelsanjaya12@gmail.com>
2025-02-21 13:57:32 +0545

    remove RentController; add email existence check in UserRepository

--- RentThis/src/main/java/com/renting/RentThis/repository/UserRepository.java
+++ RentThis/src/main/java/com/renting/RentThis/repository/UserRepository.java
@@ -1,8 +1,10 @@
 package com.renting.RentThis.repository;
 
 import com.renting.RentThis.entity.User;
+import jakarta.validation.constraints.Email;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User , Long> {
 
+    boolean existsByEmail(String email);
 }
