2b57050afe138b64d05db381e3c79372360d467f
Sanjaya <poudelsanjaya12@gmail.com>
2025-02-21 20:29:17 +0545

    Exception throw JSON

--- RentThis/src/main/java/com/renting/RentThis/exception/GlobalExceptionHandler.java
+++ RentThis/src/main/java/com/renting/RentThis/exception/GlobalExceptionHandler.java
@@ -1,14 +1,20 @@
 package com.renting.RentThis.exception;
-
+import com.renting.RentThis.dto.response.ApiResponse;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 @ControllerAdvice
 public class GlobalExceptionHandler {
     @ExceptionHandler(RuntimeException.class)
-    public ResponseEntity<?>handleRuntimeException(RuntimeException ex){
+    public ResponseEntity<ApiResponse<String>>handleRuntimeException(RuntimeException ex){
+        ApiResponse<String> errorResponse = ApiResponse.<String>builder()
+                .success(false)
+                .data(null)
+                .message(ex.getMessage())
+                .build();
+
         return ResponseEntity
                 .badRequest()
-                .body(ex.getMessage());
+                .body(errorResponse);
     }
 }
