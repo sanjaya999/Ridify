590b8641a070b21e7b1ea27f3a4c8ba26e45b8ab
Sanjaya <poudelsanjaya12@gmail.com>
2025-02-21 13:57:12 +0545

    add user registration endpoint in UserController

--- RentThis/src/main/java/com/renting/RentThis/controller/UserController.java
+++ RentThis/src/main/java/com/renting/RentThis/controller/UserController.java
@@ -1,4 +1,33 @@
 package com.renting.RentThis.controller;
 
+import com.renting.RentThis.dto.request.UserRegistrationRequest;
+import com.renting.RentThis.dto.response.ApiResponse;
+import com.renting.RentThis.dto.response.UserResponse;
+import com.renting.RentThis.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/users")
+@RequiredArgsConstructor
 public class UserController {
+    @Autowired
+    private final UserService userService;
+
+    @PostMapping("/register")
+    public ResponseEntity<ApiResponse <UserResponse>> registerUser(@RequestBody UserRegistrationRequest request){
+        UserResponse user = userService.registerUser(request);
+
+        return ResponseEntity.ok(ApiResponse.<UserResponse>builder()
+                .success(true)
+                .data(user)
+                .build()
+        );
+    }
+
 }
