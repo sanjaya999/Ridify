a03a0bd78c0c1c8136c53b6d741a7ecf3c863763
Sanjaya <poudelsanjaya12@gmail.com>
2025-02-21 13:57:27 +0545

    remove RentController; add global exception handling and email existence check in UserRepository

--- RentThis/src/main/java/com/renting/RentThis/exception/GlobalExceptionHandler.java
+++ RentThis/src/main/java/com/renting/RentThis/exception/GlobalExceptionHandler.java
@@ -1,4 +1,14 @@
 package com.renting.RentThis.exception;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+@ControllerAdvice
 public class GlobalExceptionHandler {
+    @ExceptionHandler(RuntimeException.class)
+    public ResponseEntity<?>handleRuntimeException(RuntimeException ex){
+        return ResponseEntity
+                .badRequest()
+                .body(ex.getMessage());
+    }
 }
