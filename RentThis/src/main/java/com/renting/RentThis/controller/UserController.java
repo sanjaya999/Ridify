859578f314ec22e7d8e83cb61bb840f4a21e95c2
Sanjaya <poudelsanjaya12@gmail.com>
2025-02-21 20:30:38 +0545

    @Autowired remove

--- RentThis/src/main/java/com/renting/RentThis/controller/UserController.java
+++ RentThis/src/main/java/com/renting/RentThis/controller/UserController.java
@@ -1,5 +1,4 @@
 package com.renting.RentThis.controller;
-
 import com.renting.RentThis.dto.request.UserRegistrationRequest;
 import com.renting.RentThis.dto.response.ApiResponse;
 import com.renting.RentThis.dto.response.UserResponse;
@@ -12,11 +11,12 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+
 @RestController
 @RequestMapping("/api/v1/users")
 @RequiredArgsConstructor
 public class UserController {
-    @Autowired
+
     private final UserService userService;
 
     @PostMapping("/register")
@@ -29,5 +29,4 @@ public class UserController {
                 .build()
         );
     }
-
 }
