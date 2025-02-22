ab5615fc7bcb1b7cb46e84b166b8b61469e87c22
Sanjaya <poudelsanjaya12@gmail.com>
2025-02-22 07:31:29 +0545

    add security configuration with HTTP security and password encoder

---
+++ RentThis/src/main/java/com/renting/RentThis/config/SecurityConfig.java
@@ -0,0 +1,31 @@
+package com.renting.RentThis.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> {
+                    auth.requestMatchers("/api/v1/users/register", "/api/v1/users/login").permitAll()
+                            .anyRequest().authenticated();
+                })
+                .formLogin(form -> form.disable())
+                .build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
