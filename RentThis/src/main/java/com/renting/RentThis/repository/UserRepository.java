aaba55667c89478a34834e322540c477f9c48e3c
Sanjaya <poudelsanjaya12@gmail.com>
2025-02-23 12:05:09 +0545

    add UserRepository method to find user by email and implement login functionality in UserService

--- RentThis/src/main/java/com/renting/RentThis/repository/UserRepository.java
+++ RentThis/src/main/java/com/renting/RentThis/repository/UserRepository.java
@@ -3,8 +3,13 @@ package com.renting.RentThis.repository;
 import com.renting.RentThis.entity.User;
 import jakarta.validation.constraints.Email;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
+@Repository
 public interface UserRepository extends JpaRepository<User , Long> {
 
     boolean existsByEmail(String email);
+    Optional<User> findByEmail(String email);
 }
--- RentThis/src/main/java/com/renting/RentThis/service/UserService.java
+++ RentThis/src/main/java/com/renting/RentThis/service/UserService.java
@@ -1,8 +1,11 @@
 package com.renting.RentThis.service;
 
+import com.renting.RentThis.dto.request.LoginRequest;
 import com.renting.RentThis.dto.request.UserRegistrationRequest;
+import com.renting.RentThis.dto.response.LoginResponse;
 import com.renting.RentThis.dto.response.UserResponse;
 import com.renting.RentThis.entity.User;
+import com.renting.RentThis.repository.TokenRepository;
 import com.renting.RentThis.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -13,6 +16,8 @@ import org.springframework.stereotype.Service;
 public class UserService {
     private  final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
+    private  final JwtService jwtService;
+    private final TokenRepository tokenRepository;
 
     public  UserResponse registerUser(UserRegistrationRequest request){
 
@@ -26,10 +31,37 @@ public class UserService {
         user.setPassword(encodedPassword);
         User savedUser = userRepository.save(user);
 
+
+
         return UserResponse.builder()
                 .id(savedUser.getId())
                 .email(savedUser.getEmail())
                 .name(savedUser.getName())
+                .build();
+    }
+    public LoginResponse loginUser(LoginRequest request){
+
+        if (!userRepository.existsByEmail(request.getEmail())) {
+            throw new RuntimeException("Email does not exist");
+        }
+        User user = userRepository.findByEmail(request.getEmail()).get();
+
+        if(!passwordEncoder.matches(request.getPassword(), user.getPassword())){
+            throw new RuntimeException("Invalid password");
+        }
+
+        tokenRepository.deleteByUser(user);
+
+        String accessToken = jwtService.generateAccessToken(user.getId() , user.getEmail());
+        String refreshToken = jwtService.generateRefreshToken(user.getId(),  user.getEmail());
+
+
+        return LoginResponse.builder()
+                .id(user.getId())
+                .name(user.getName())
+                .email(user.getEmail())
+                .accessToken(accessToken)
+                .refreshToken(refreshToken)
                 .build();
     }
 
