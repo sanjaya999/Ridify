dd395028a1423bcd65f6397dc14d902be6e8fba2
Sanjaya <poudelsanjaya12@gmail.com>
2025-02-23 12:04:55 +0545

    add LoginRequest and LoginResponse DTOs for authentication

---
+++ RentThis/src/main/java/com/renting/RentThis/dto/response/LoginResponse.java
@@ -0,0 +1,15 @@
+package com.renting.RentThis.dto.response;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class LoginResponse {
+    private Long id;
+    private String name;
+    private String email;
+    private String accessToken;
+    private String refreshToken;
+
+}
