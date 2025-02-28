62e04bddf86679bb6068a700368b21745e7ce131
Sanjaya <poudelsanjaya12@gmail.com>
2025-02-28 11:58:22 +0545

    refactor SecurityConfig to implement JWT authentication and configure stateless session management

--- RentThis/src/main/java/com/renting/RentThis/config/SecurityConfig.java
+++ RentThis/src/main/java/com/renting/RentThis/config/SecurityConfig.java
@@ -1,17 +1,25 @@
 package com.renting.RentThis.config;
 
+import com.renting.RentThis.filter.JwtAuthenticationFilter;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
+    private  final JwtAuthenticationFilter jwtAuthFilter;
+
+
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -21,6 +29,8 @@ public class SecurityConfig {
                     auth.requestMatchers("/api/v1/users/**").permitAll()
                             .anyRequest().authenticated();
                 })
+                .sessionManagement(session ->session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .addFilterBefore(jwtAuthFilter , UsernamePasswordAuthenticationFilter.class)
                 .formLogin(form -> form.disable())
                 .build();
     }
