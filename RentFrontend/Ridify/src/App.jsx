0f8e8263218c4535f0a6ec6ffe22b7b6e68f943b
Sanjaya <poudelsanjaya12@gmail.com>
2025-02-28 11:58:02 +0545

    refactor routing logic in App component to conditionally navigate based on authentication status

--- RentFrontend/Ridify/src/App.jsx
+++ RentFrontend/Ridify/src/App.jsx
@@ -1,20 +1,26 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Navigate, useLocation } from 'react-router-dom';
-import Login from './component/Login.jsx';
-import Register from './component/Register.jsx';
-import Home from './component/Home.jsx';
-import ProtectedRoute from './component/ProtectedRoute.jsx';
-import "./Style/Login.css"
-import "./Style/Register.css"
-import "./Style/Home.css"
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+  useLocation,
+} from "react-router-dom";
+import Login from "./component/Login.jsx";
+import Register from "./component/Register.jsx";
+import Home from "./component/Home.jsx";
+import ProtectedRoute from "./component/ProtectedRoute.jsx";
+import "./Style/Login.css";
+import "./Style/Register.css";
+import "./Style/Home.css";
 
 // Message component for displaying redirects
 const Message = () => {
   const location = useLocation();
   const message = location.state?.message;
-  
+
   if (!message) return null;
-  
+
   return (
     <div className="message-container">
       <div className="message">{message}</div>
@@ -29,19 +35,28 @@ function App() {
         <Message />
         <div className="auth-container">
           <Routes>
-            <Route path="/login" element={<Login />} />
+            <Route
+              path="/login"
+              element={
+                localStorage.getItem("accessToken") ? (
+                  <Navigate to="/home" replace />
+                ) : (
+                  <Login />
+                )
+              }
+            />
             <Route path="/register" element={<Register />} />
-            <Route 
-              path="/home" 
+            <Route
+              path="/home"
               element={
                 <ProtectedRoute>
                   <Home />
                 </ProtectedRoute>
-              } 
+              }
             />
             {/* Redirect to login by default */}
             <Route path="/" element={<Navigate to="/login" replace />} />
-            {/* Catch all invalid routes */}
+            {/* catch invalid route */}
             <Route path="*" element={<Navigate to="/login" replace />} />
           </Routes>
         </div>
