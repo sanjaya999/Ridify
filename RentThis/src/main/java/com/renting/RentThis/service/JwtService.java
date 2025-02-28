4d3d56fbfd8c2575dd86728ccccb73b1ec18fd36
Sanjaya <poudelsanjaya12@gmail.com>
2025-02-28 11:58:33 +0545

    refactor JwtService to enhance token claim extraction and validation logic

--- RentThis/src/main/java/com/renting/RentThis/service/JwtService.java
+++ RentThis/src/main/java/com/renting/RentThis/service/JwtService.java
@@ -1,9 +1,9 @@
 package com.renting.RentThis.service;
 
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.JwtException;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
-import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -13,20 +13,38 @@ import java.security.Key;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.function.Function;
 
 @Service
 public class JwtService {
     @Value("${jwt.access.secret}")
-    private  String accessSecret;
+    private String accessSecret;
 
     @Value("${jwt.access.expiration}")
     private Long accessExpiration;
 
     @Value("${jwt.refresh.secret}")
-    private  String refreshSecret;
+    private String refreshSecret;
 
     @Value("${jwt.refresh.expiration}")
     private Long refreshExpiration;
+
+    public String extractEmail(String token){
+        return extractClaim(token , accessSecret , Claims::getSubject);
+    }
+    public <T>T extractClaim(String token , String secret , Function<Claims , T> claimsResolver){
+        final Claims claims = extractAllClaims(token,secret);
+        return claimsResolver.apply(claims);
+    }
+
+    public Claims extractAllClaims (String token , String secret){
+        return  Jwts
+                .parserBuilder()
+                .setSigningKey(getSignInKey(secret))
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+    }
 
     public String generateAccessToken(Long userId, String email) {
         return generateToken(userId, email, accessSecret, accessExpiration);
@@ -36,7 +54,7 @@ public class JwtService {
         return generateToken(userId, email, refreshSecret, refreshExpiration);
     }
 
-    public String generateToken(Long userId, String email , String secret, Long expiration) {
+    public String generateToken(Long userId, String email, String secret, Long expiration) {
         Map<String, Object> claims = new HashMap<>();
         claims.put("userId", userId);
         claims.put("email", email);
@@ -49,20 +67,20 @@ public class JwtService {
                 .signWith(getSignInKey(secret), SignatureAlgorithm.HS256)
                 .compact();
     }
-
-    private Key getSignInKey(String secret){
+    private Key getSignInKey(String secret) {
         return Keys.hmacShaKeyFor(secret.getBytes());
     }
 
-    public boolean validateToken (String token ,String Secret){
-        try{
+
+    public boolean validateToken(String token, UserDetails userDetails) {
+        final  String  email = extractEmail(token);
+        try {
             Jwts.parserBuilder()
-                    .setSigningKey(getSignInKey(Secret))
+                    .setSigningKey(getSignInKey(accessSecret))
                     .build()
                     .parseClaimsJws(token);
-            return true;
-
-        }catch (JwtException | IllegalArgumentException e){
+            return (email.equals(userDetails.getUsername()));
+        } catch (JwtException | IllegalArgumentException e) {
             return false;
         }
     }
