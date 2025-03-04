e30b7e0ad3addd06da2825f03bf5d67fd4c2b5e5
Sanjaya Poudel <poudelsanjaya12@gmail.com>
2025-03-04 14:27:41 +0545

    add role-based authority handling in UserDetailsService for enhanced security

--- RentThis/src/main/java/com/renting/RentThis/service/UserDetailsService.java
+++ RentThis/src/main/java/com/renting/RentThis/service/UserDetailsService.java
@@ -3,12 +3,17 @@ package com.renting.RentThis.service;
 import com.renting.RentThis.entity.User;
 import com.renting.RentThis.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import java.nio.file.attribute.UserPrincipal;
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -21,9 +26,10 @@ public class UserDetailsService implements org.springframework.security.core.use
         User user = userRepository.findByEmail(email)
                 .orElseThrow(()-> new UsernameNotFoundException("No user found with email" + email ));
 
+        GrantedAuthority authority = new SimpleGrantedAuthority("ROLE_" + user.getRole());
         return new org.springframework.security.core.userdetails.User(
                 user.getEmail(),
                 user.getPassword(),
-                new ArrayList<>()
+                Collections.singletonList(authority)
         );    }
 }
