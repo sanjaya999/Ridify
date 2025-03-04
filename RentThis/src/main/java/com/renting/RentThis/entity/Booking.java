8e7686b050dc06bc928b2e42881584d49c4b3b7e
Sanjaya Poudel <poudelsanjaya12@gmail.com>
2025-03-04 12:08:23 +0545

    add Booking entity with relationships to User and Vehicle, including time and status fields

---
+++ RentThis/src/main/java/com/renting/RentThis/entity/Booking.java
@@ -0,0 +1,30 @@
+package com.renting.RentThis.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Data
+@Table(name = "bookings")
+public class Booking {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private  Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "vehicle_id")
+    private Vehicle vehicle;
+
+    private LocalDateTime startTime;
+    private LocalDateTime endTime;
+
+    private String status;
+
+}
