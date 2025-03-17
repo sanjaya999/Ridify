389414cf85e4cc342a068ae7e2a68c5209ff1393
Sanjaya Poudel <poudelsanjaya12@gmail.com>
2025-03-17 18:03:15 +0545

    implement vehicle fetching and display in Landing component

--- RentFrontend/Ridify/src/component/Landing.jsx
+++ RentFrontend/Ridify/src/component/Landing.jsx
@@ -1,11 +1,44 @@
 import { Link } from 'react-router-dom';
 import '../Style/Landing.css';
+import { get } from '../api/api';
+import { useQuery } from '@tanstack/react-query';
 
 function Landing() {
+  const baseUrl = "http://localhost:8080"; // Backend base URL
+
+  
+  const  fetchVehicles = async()=>{
+    const {data} = await get(`vehicles/getAll`);
+    console.log(data);
+    return data;
+  }
+  const { data, isLoading, isError, error } = useQuery({
+    queryKey: ['vehicles'],
+    queryFn: fetchVehicles,
+  });
+  if (isLoading) return <div>Loading...</div>;
+  if (isError) return <div>Error: {error.message}</div>;
+
+
 return(
-  <>
-  this is landing page
-  </>
+  <div>
+      <h1>Vehicle List</h1>
+      <ul>
+        {data.map(vehicle => (
+          <li key={vehicle.id}>
+            <h2>{vehicle.name}</h2>
+            <p>Model: {vehicle.model}</p>
+            <p>Type: {vehicle.type}</p>
+            <p>Plate Number: {vehicle.plateNum}</p>
+            <p>Owner: {vehicle.ownerName}</p>
+            {console.log(baseUrl/+vehicle.photoUrl)}
+            <img 
+              src={`${baseUrl}/${vehicle.photoUrl}`} // Construct the full image URL
+              alt={vehicle.name} style={{ width: '200px', height: 'auto' }} />
+          </li>
+        ))}
+      </ul>
+    </div>
 )  
 }
 
