b767994bbec95345156bd1c7f27a0fb343e0b438
Sanjaya Poudel <poudelsanjaya12@gmail.com>
2025-04-01 14:53:21 +0545

    Add method to VehicleRepository for retrieving vehicles by owner ID

--- RentThis/src/main/java/com/renting/RentThis/repository/VehicleRepository.java
+++ RentThis/src/main/java/com/renting/RentThis/repository/VehicleRepository.java
@@ -1,10 +1,15 @@
 package com.renting.RentThis.repository;
 
 import com.renting.RentThis.entity.Vehicle;
+import com.renting.RentThis.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
-public interface VehicleRepository extends JpaRepository<Vehicle , Long> {
+public interface VehicleRepository extends JpaRepository<Vehicle, Long> {
+    List<Vehicle> findByOwnerId(Long ownerId);
+
 
 }
