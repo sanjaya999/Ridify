c2b278c8f1bc1352c3c0a0b2117915ef5df0d6b5
Sanjaya Poudel <poudelsanjaya12@gmail.com>
2025-04-12 20:14:50 +0545

    feat: Add booking verification and confirmation methods in BookingController

--- RentThis/src/main/java/com/renting/RentThis/controller/BookingController.java
+++ RentThis/src/main/java/com/renting/RentThis/controller/BookingController.java
@@ -1,8 +1,10 @@
 package com.renting.RentThis.controller;
 
+import com.renting.RentThis.dto.request.BookingConfirmRequest;
 import com.renting.RentThis.dto.request.BookingRequest;
 import com.renting.RentThis.dto.response.ApiResponse;
 import com.renting.RentThis.dto.response.BookingResponse;
+import com.renting.RentThis.dto.response.BookingVerificationResponse;
 import com.renting.RentThis.dto.response.VehicleResponse;
 import com.renting.RentThis.service.BookingService;
 import lombok.RequiredArgsConstructor;
@@ -20,8 +22,8 @@ public class BookingController {
     private final BookingService bookingService;
 
     @PostMapping("/confirmBooking")
-    public ResponseEntity<ApiResponse<BookingResponse>> bookVehicle(@RequestBody BookingRequest request ){
-        System.out.println(request.getStartTime() + " end time : "+request.getEndTime());
+    public ResponseEntity<ApiResponse<BookingResponse>> bookVehicle(@RequestBody BookingRequest request) {
+        System.out.println(request.getStartTime() + " end time : " + request.getEndTime());
         BookingResponse bookingResponse = bookingService.addBooking(request);
 
         return ResponseEntity.ok(ApiResponse.<BookingResponse>builder()
@@ -32,20 +34,20 @@ public class BookingController {
     }
 
     @GetMapping("/currentUserBooking")
-    public ResponseEntity<ApiResponse<List<BookingResponse>>> currentUserBookings(){
-       List<BookingResponse> bookingResponse = bookingService.getCurrentUserBooking();
+    public ResponseEntity<ApiResponse<List<BookingResponse>>> currentUserBookings() {
+        List<BookingResponse> bookingResponse = bookingService.getCurrentUserBooking();
 
-       return ResponseEntity.ok(ApiResponse.<List<BookingResponse>>builder()
-               .success(true)
-               .status(200)
-               .data(bookingResponse)
-               .message("success")
-               .build());
+        return ResponseEntity.ok(ApiResponse.<List<BookingResponse>>builder()
+                .success(true)
+                .status(200)
+                .data(bookingResponse)
+                .message("success")
+                .build());
 
     }
 
     @GetMapping("/{vehicleId}/bookings")
-    public ResponseEntity<ApiResponse<List<BookingResponse>>> allBookingOfOneVehicle(@PathVariable  Long vehicleId){
+    public ResponseEntity<ApiResponse<List<BookingResponse>>> allBookingOfOneVehicle(@PathVariable Long vehicleId) {
 
         List<BookingResponse> bookingResponse = bookingService.allBookingOfOneVehicle(vehicleId);
 
@@ -56,4 +58,17 @@ public class BookingController {
                 .message("success")
                 .build());
     }
+
+    @PostMapping("/verify")
+    public ResponseEntity<BookingVerificationResponse> verifyBooking(@RequestBody BookingRequest request) {
+        BookingVerificationResponse response = bookingService.verifyTime(request);
+        return ResponseEntity.ok(response);
+    }
+
+    @PostMapping("/confirm")
+    public ResponseEntity<BookingResponse> confirmBooking(@RequestBody BookingConfirmRequest confirmRequest) {
+        BookingResponse booking = bookingService.confirmBooking(confirmRequest.getToken());
+        return ResponseEntity.ok(booking);
+    }
+
 }
