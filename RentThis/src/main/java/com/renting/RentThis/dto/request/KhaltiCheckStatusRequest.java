c241ad2be2199d429fb2402cb98991d4cb5cd147
Sanjaya Poudel <poudelsanjaya12@gmail.com>
2025-04-21 18:01:24 +0545

    feat: Add KhaltiCheckStatusRequest DTO for payment status requests

---
+++ RentThis/src/main/java/com/renting/RentThis/dto/request/KhaltiCheckStatusRequest.java
@@ -0,0 +1,10 @@
+package com.renting.RentThis.dto.request;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+public class KhaltiCheckStatusRequest {
+    private String pidx;
+    private String token;
+}
