ac32b9286beadcfa8398b33d4697ea861d97762a
Sanjaya Poudel <poudelsanjaya12@gmail.com>
2025-05-07 20:03:22 +0545

    feat: add methods for vehicle name search in VehicleRepository

--- RentThis/src/main/java/com/renting/RentThis/repository/VehicleRepository.java
+++ RentThis/src/main/java/com/renting/RentThis/repository/VehicleRepository.java
@@ -3,6 +3,8 @@ package com.renting.RentThis.repository;
 import com.renting.RentThis.entity.Vehicle;
 import com.renting.RentThis.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -12,4 +14,10 @@ public interface VehicleRepository extends JpaRepository<Vehicle, Long> {
     List<Vehicle> findByOwnerId(Long ownerId);
 
 
+    List<Vehicle> findByName(String searchTerm);
+
+    List<Vehicle> findByNameContainingIgnoreCase(String searchTerm);
+
+    @Query(value = "SELECT * FROM vehicle v WHERE REPLACE(LOWER(v.name), ' ', '') LIKE CONCAT('%', REPLACE(LOWER(:searchTerm), ' ', ''), '%')", nativeQuery = true)
+    List<Vehicle> findByNameIgnoringSpaces(@Param("searchTerm") String searchTerm);
 }
